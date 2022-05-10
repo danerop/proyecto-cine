558e6ab4dfd2bda73c9a99b4c9da143161323f42
Elian Dominguez <56704805+elian-dominguez@users.noreply.github.com>
2022-05-10 18:11:47 -0300

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -4,3 +4,4 @@ INSERT INTO pelicula(nombre, anio, descripcion, duracion, urlImagenPelicula)
 VALUES("Sonic 2", 2022, "descripcion", 150, "img/pelicula/sonic2.jpg"),
 	  ("Dr Strange: Multiverso de la locura", 2022, "descripcion", 150, "img/pelicula/drstrange.jpg"),
       ("Animales Fantasticos 3", 2022, "descripcion", 150, "img/pelicula/animalesf.jpg");
+      
